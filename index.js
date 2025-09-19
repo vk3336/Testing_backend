dce29362b2fd765258c502b6cf1d8f7ee566e009
urvikalal <urvi.qodic@gmail.com>
2025-09-19 12:10:48 +0530

    made change in the index.js as to remove the node_env

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === "production",
+      secure: false, // Allow cookies over HTTP
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     },
