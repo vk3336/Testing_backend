5411c8c225eb55932c33f712f4406c86e2c142c6
urvikalal <urvi.qodic@gmail.com>
2025-12-25 16:35:11 +0530

    done

--- model/cartModel.js
+++ model/cartModel.js
@@ -8,7 +8,7 @@ const cartSchema = new mongoose.Schema({
     },
     productId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Product',
+        ref: 'Producttesting',
         required: true
     },
     quantity: {
